bce860d91682bd52fd58301831bb5c392ebc541b
AnasMouak <anasmouak1111@gmail.com>
2023-11-14 16:06:39 -0800

    prcnt

---
+++ prntPRCNT.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * prntPrcnt - Prints the percent symbol (%).
+ * Return: Always returns 1, representing one character printed.
+ */
+int prntPrcnt(void)
+{
+	/* 'prcnt' is the ASCII value of the character '%' */
+	int prcnt = 37;
+
+	_putchar(prcnt);
+	return (1);
+}
